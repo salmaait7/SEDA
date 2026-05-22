4a52a71b03a418486b9aca15d02c853fbdf2686a
Salma <salmaaitbenmbark@gmail.com>
2026-05-22 12:00:09 +0200

    fix Gates initialization

--- lib/seda/synthesizer/GateFactory.rb
+++ lib/seda/synthesizer/GateFactory.rb
@@ -2,24 +2,46 @@ module Seda
   module Gtech
 
     class And2 < Gate2
+      def initialize
+        super("AND2_GATE")
+      end
     end
 
     class Or2 < Gate2
+      def initialize
+        super("OR2_GATE")
+      end
     end
 
     class Nand2 < Gate2
+      def initialize
+        super("NAND2_GATE")
+      end
     end
 
     class Nor2 < Gate2
+      def initialize
+        super("NOR2_GATE")
+      end
     end
 
     class Xor2 < Gate2
+      def initialize
+        super("XOR2_GATE")
+      end
     end
 
     class Buf < Gate1
+      def initialize
+        super("BUF_GATE")
+      end
     end
 
     class Not < Gate1
+      def initialize
+        super("NOT_GATE")
+      end
     end
+
   end
 end
