68deca248326806d95c60159644e79f4f54a4335
salmaait7 <salmaaitbenmbark@gmail.com>
2026-07-01 10:52:32 +0200

    fix random expr generation

--- lib/seda/random_generation/expr_rand_gen.rb
+++ lib/seda/random_generation/expr_rand_gen.rb
@@ -1,6 +1,6 @@
 require_relative "expr_ast"
 
-#generates random expressions with controlled dpth
+# generates random expressions with controlled depth
 module Seda
   class ExprRandGen
     EXPR_KIND = {
@@ -8,22 +8,25 @@ module Seda
       2 => [Expr::And, Expr::Or, Expr::Xor, Expr::Nand, Expr::Nor]
     }
 
-    def initialize(vars:, depth:, nb_outputs:, reuse_probability: 0.5)
+    def initialize(vars:, depth:, nb_outputs:)
       @vars = vars.map { |v| Expr::Var.new(v) }
       @depth = depth
       @nb_outputs = nb_outputs
-      @reuse_probability = reuse_probability
-      @expr_pool = [] # to store generated expressions for potential reuse
+      @expr_pool = []
       @unused_vars = []
     end
 
     def generate
       expressions = []
-      @expr_pool = [] # pool of previously generated expressions for reuse
-      @unused_vars = @vars.shuffle # inputs that must be used at least once
+      @expr_pool = []
+      @unused_vars = @vars.shuffle 
+
+      @common_depth = rand(1..@depth)
+      puts "common depth: #{@common_depth}"
+      common_expr = gen_common_expr(@common_depth)
 
       @nb_outputs.times do
-        expressions << gen_expr(@depth, top_level: true)
+        expressions << extend_path_from_common(common_expr)
       end
 
       if @unused_vars.any?
@@ -34,75 +37,49 @@ module Seda
       expressions
     end
 
-    def gen_expr(depth, top_level: false)
-      if depth == 0
-        return choose_var
-      end
+    def gen_common_expr(depth)
+      return choose_var if depth <= 0
 
-      # to prevent that some inputs are never used, we force the generator to use them until they are all used at least once
-      if !top_level && @unused_vars.empty? && !@expr_pool.empty? && rand < @reuse_probability
-        candidates = @expr_pool.select { |expr| expr_depth(expr) <= depth }
+      gate_kind = EXPR_KIND[2].sample
+      gate_kind_unary = EXPR_KIND[1].sample
 
-        if candidates.any?
-          return candidates.sample
-        end
-      end
-
-      arity = choose_arity
-      gate_kind = EXPR_KIND[arity].sample
-
-      expr =
-        if arity == 1
-          gate_kind.new(gen_expr(depth - 1))
+      if depth == 1
+        gate_kind.new(choose_var, choose_var)
+      else
+        if rand < 0.5
+          gate_kind.new(gen_common_expr(depth - 1), choose_var)
         else
-          gate_kind.new(gen_expr(depth - 1), gen_expr(depth - 1)) # it generates per example (x0 And (Not x1))
+          gate_kind_unary.new(gen_common_expr(depth - 1))
         end
+      end
+    end
 
-      remember_expr(expr)
+    def extend_path_from_common(common_expr)
+      path_length = @depth - @common_depth
+      expr = common_expr
+
+      path_length.times do
+        gate_kind = EXPR_KIND[2].sample
+        gate_kind_unary = EXPR_KIND[1].sample
+        side_expr = choose_var
+
+        expr =
+          if rand < 0.3
+            gate_kind_unary.new(expr)
+          else
+            gate_kind.new(side_expr, expr)
+          end
+      end
 
       expr
     end
 
     def choose_var
-      # first use inputs that have not appeared yet
-      # after that, choose randomly from all inputs
       if @unused_vars.any?
         @unused_vars.shift
       else
         @vars.sample
       end
-    end
-
-    def choose_arity
-      if @unused_vars.any?
-        2
-      else
-        [1, 2].sample
-      end
-    end
-
-    def remember_expr(expr)
-      return if expr.is_a?(Expr::Var) # don't store variables in the pool
-
-      @expr_pool << expr unless @expr_pool.include?(expr) # avoid duplicat
-    end
-
-    def expr_depth(expr)
-      return 0 if expr.is_a?(Expr::Var)
-
-      children = expr.instance_variables.map do |var|
-        value = expr.instance_variable_get(var)
-
-        if value.is_a?(Expr::Var) || value.class.name.include?("Expr::")
-          value
-        else
-          nil
-        end
-      end.compact
-
-      return 1 if children.empty?
-
-      1 + children.map { |child| expr_depth(child) }.max
     end
   end
 end
